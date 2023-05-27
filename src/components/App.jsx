8173c3de9cdfa4eab500d97a94288c5afe762fb9
Olia Rosiak <olia.rosiak@gmail.com>
2023-05-27 16:37:09 +0300

    spike render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import ContactForm from './contactForm/ContactForm';
+// import ContactForm from './contactForm/ContactForm';
 import ContactList from './contactList/ContactList';
 
 class App extends Component{
@@ -11,33 +11,45 @@ class App extends Component{
     number: ''
   }
 
-  render() {
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          // display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 25,
-          color: '#010101'
-        }}
-      >
-        
-        <h1>Phonebook</h1>
-        <ContactForm />
-        {/* <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        /> */}
+  // handleChange = evt => {
+  //   const { name, value } = evt.target;
+  //   this.setState({ [name]: value });      
+  // }; 
+  
+  handleState = (foobar) => {
+    this.setState({ name: foobar });
+  }
 
+  handleSubmitContact = evt => {
+    evt.preventDefault();   
+    const { name } = evt.target.elements;
+    const contactName = name.value;
+    console.log(contactName);
+    this.handleState(contactName);
+
+    // this.props.onSubmit({ ...this.state });
+  }
+
+  render() {
+
+    const { name } = this.state;
+    console.log('name render',name);
+
+    return (
+      <div>        
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmitContact} >
+          <label>
+            <input type="text" name="name" required/>
+          </label>
+          <button type="submit">Add Contact</button>
+        </form>
+
+        {/* <ContactForm formSubmit={this.handleSubmitContact} /> */}
+        {/* <ContactForm formSubmit={this.handleSubmitContact} onSubmit={values => console.log(values)} /> */}
         <h2>Contacts</h2>       
-      {/* <Filter /> */}
-       <ContactList name='Nika' number='124563221588'/>
-        
+        {/* <Filter /> */}
+        <ContactList name={name} number='124563221588'/>        
       </div>
     )
   }  
@@ -45,3 +57,4 @@ class App extends Component{
 
 export default App;
 
+// value={name} onChange={this.handleChange}
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,17 +1,24 @@
 
-const ContactForm = () => (
-    <form>
-        <label>
-            Name
-            <input
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-            />
-        </label>
-        <label>
+// const ContactForm = ({ formSubmit }) => (
+//     <form onSubmit={()=>formSubmit()}>
+//         <label>
+//             Name
+//             <input
+//                 type="text"
+//                 name="name"
+//                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                 required
+//             />
+//         </label>
+
+//         <button type="submit">Add Contact</button>
+//     </form>
+// )
+
+// export default ContactForm;
+
+        {/* <label>
             Phone number
             <input
                 type="tel"
@@ -20,8 +27,4 @@ const ContactForm = () => (
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
             />
-        </label>
-    </form>
-)
-
-export default ContactForm;
+        </label> */}
