b33428d6ef21ccfe9d596c391e3d57eae84b95c3
Olia Rosiak <olia.rosiak@gmail.com>
2023-05-28 23:35:12 +0300

    step 3 hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,86 +2,63 @@ import React, { Component } from "react";
 import { nanoid } from 'nanoid';
 
 import ContactForm from './contactForm/ContactForm';
-// import Filter from './filter/Filter';
+import Filter from './filter/Filter';
 import ContactList from './contactList/ContactList';
 
 const INITIAL_STATE = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Harry Potter', number: '459-12-56' },
+      { id: 'id-2', name: 'Ronald Weasley', number: '443-89-12' },
+      { id: 'id-3', name: 'Hermione Granger', number: '745-17-79' },
+      { id: 'id-4', name: 'Rubeus Hagrid', number: '645-17-79' },
+      { id: 'id-5', name: 'Draco Malfoy', number: '227-91-26' },
     ],
     filter: '',
 };
 
 class App extends Component{
-  // state = {
-  //   // contacts: [],
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // } 
 
   state = {
     ...INITIAL_STATE
   };
 
   heandleOnFilter = evt => {
-    const { name, value } = evt.target;  
-    this.setState({ [name]: value });
-
-    const { contacts, filter } = this.state;
-
-    const filtered = contacts.filter(contact => {
-      const filterLower = filter.toLowerCase();
-      const contactNameLower = contact.name.toLowerCase();
-      
-      return contactNameLower.includes(filterLower);
-    });
-    
-    console.log(filtered);
-
-    this.setState({ contacts: [...filtered] });
-
+    const { value } = evt.target;      
+    this.setState({ filter: value });
   }
-
   
-  handleUpdateContacts = ({name, number}) => {
-    
+  handleOnSubmit = ({ name, number }) => { 
+
     const newUser = {
       id: nanoid(),
       name,
-      number
+      number,
     };
 
     this.setState({ contacts: [...this.state.contacts, newUser] });    
   }
 
-  // handleSearchField = (evt) => {
-  //   console.log(evt);
-  
-  // }
+  handleUpdateContactList = () => {
+    const { contacts, filter } = this.state;  
+
+    return contacts.filter(contact => {
+      const filterLower = filter.toLowerCase();
+      const contactNameLower = contact.name.toLowerCase();
+      
+      return contactNameLower.includes(filterLower);
+    })
+  }
   
   render() {
-    const { contacts, filter } = this.state;
+    const { filter } = this.state;  
 
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleUpdateContacts} />
+        <ContactForm onSubmit={this.handleOnSubmit} />
         <h2>Contacts</h2>
-        <input type="text" name="filter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Use only letters, apostrophe, dash and spaces."
-          value={filter} onChange={this.heandleOnFilter}
-        />
-        {/* <Filter option={this.heandleFilter} search={this.handleSearchField} /> */}
-        <ContactList contactsNames={contacts} />
+        <Filter option={filter} search={this.heandleOnFilter} />
+        <ContactList contactsNames={this.handleUpdateContactList()} />
       </div>
     )
   }  
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,29 +1,10 @@
 
-const Filter = ({ option, search }) => {
-
-
-    return (
-        <input type="text" name="filter"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Use only letters, apostrophe, dash and spaces."
-            // value={} onChange={}
-        />
-    )
-}
-// const Filter = ({search}) = (
-//     <input type="text" name="search"
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         title="Use only letters, apostrophe, dash and spaces."
-        
-//     />
-// )
-
-// const Filter = () = (
-//     <input type="text" name="search"
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         title="Use only letters, apostrophe, dash and spaces."
-        
-//     />
-// )
+const Filter = ({option, search}) => (
+    <input type="text" name="search"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Use only letters, apostrophe, dash and spaces."
+        value={ option} onChange={search}
+    />
+)
 
 export default Filter;
